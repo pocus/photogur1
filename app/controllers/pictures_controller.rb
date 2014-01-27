337bcda794e8e69d0fea62903774a6585bf86138
David Yip <dvd.yip@gmail.com>
2014-01-27 17:36:33 -0500

    moving @pictures in the controller into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,61 +2,12 @@ class PicturesController < ApplicationController
 
 	def index #this somehow automatically renders index.html
 
-		@pictures = [ #array of hashes
-
-			{
-
-				:title => "The old church",
-				:artist => "Sergey Ershov",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-
-			},
-
-			{
-				:title => "Sea power",
-				:artist => "Stephen Schullion",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-
-			},
-
-			{
-				:title => "Into the poppies",
-				:artist => "John Wilhelm",
-				:url =>  "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-			}
-
-		]
+		@pictures = Picture.all #.all --- probably in the activerecord:base? see railsguides s5.2
 
 	end
 
 	def show
-
-				@pictures = [ #array of hashes
-
-			{
-
-				:title => "The old church",
-				:artist => "Sergey Ershov",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-
-			},
-
-			{
-				:title => "Sea power",
-				:artist => "Stephen Schullion",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-
-			},
-
-			{
-				:title => "Into the poppies",
-				:artist => "John Wilhelm",
-				:url =>  "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-			}
-
-		]
-		@picture = @pictures[params[:id].to_i] #takes the id that was given by the route and sends to view
-		# if params[:id] =1, then @picture = @pictures[1]
+		@picture = Picture.find(params[:id]) #.find --- also in the activerecord:base?
 	end
 
 	def new
@@ -72,27 +23,32 @@ end
 
 
 
-	# def picture0 #this looks for something called picture0.html.
-	# 	@picture = {
-	# 		:title => "The old church",
-	# 		:artist => "Sergey Ershov",
-	# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	# 	}
-	# end
+	# def show ((#old show data stored in hashes in the controller))
 
-	# def picture1
-	# 	@picture = {
+	# 			@pictures = [ #array of hashes
+
+	# 		{
+
+	# 			:title => "The old church",
+	# 			:artist => "Sergey Ershov",
+	# 			:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+
+	# 		},
+
+	# 		{
 	# 			:title => "Sea power",
 	# 			:artist => "Stephen Schullion",
 	# 			:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	# 	}
-	# end
 
+	# 		},
 
-	# def picture2
-	# 	@picture = {
+	# 		{
 	# 			:title => "Into the poppies",
 	# 			:artist => "John Wilhelm",
 	# 			:url =>  "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	# 	}
+	# 		}
+
+	# 	]
+	# 	@picture = @pictures[params[:id].to_i] #takes the id that was given by the route and sends to view
+	# 	# if params[:id] =1, then @picture = @pictures[1]
 	# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Picture.create!(
+	:title => "The old church",
+	:artist => "Sergey",
+	:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+	)
+
+#call destructive class method "create" in the Picture class and pass three hashes as parameters.
+#creates a new instance of the model Picture - remember we created picture.rb as a model
+
+
+Picture.create!(
+	:title => "Sea power",
+	:artist =>"Stephen Scullion",
+	:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+	)
+
+Picture.create!(
+	:title => "Into the poppies",
+	:artist => "John Wilherm",
+	:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+	)
